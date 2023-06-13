0704f189a4192aea9850599417ca77c3d2f26588
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-06-12 18:27:17 -0700

    pal

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
 /**
-* is_palindrome - frees a listint_t list
-* @head: pointer to list to be freed
-* Return: 1 if it is palidrome, 0 if it is not
-*/
+ * is_palindrome - checks for list is palindrome
+ * @head: head of linked list
+ * Return:  boolean
+ */
+
 int is_palindrome(listint_t **head)
 {
-	int len = 0, i;
-	listint_t *h;
-	int aux[1000000];
+	int len = 0, i = 0;
+	listint_t *tmp;
+	int *Ns;
 
-	h = *head;
-	if (!h)
+	tmp = *head;
+	if ((*head) == NULL)
 		return (1);
-	while (h)
+	while (tmp->next != NULL)
 	{
-		aux[len] = h->n;
-		h = h->next;
 		len++;
+		tmp = tmp->next;
 	}
-	for (i = 0; i < len; i++)
+	if (len == 1)
+		return (1);
+	Ns = malloc(sizeof(int) * len);
+	tmp = *head;
+	while (tmp != NULL)
 	{
-		if (aux[i] != aux[len - 1 - i])
+		Ns[i] = tmp->n;
+		tmp = tmp->next;
+		i++;
+	}
+	for (i = 0; i <= len / 2; i++)
+	{
+		if(Ns[i] != Ns[len - i])
 			return (0);
 	}
 	return (1);
