bd3472fae91308d6695d9b8e72407da0e34c8870
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-06-12 18:31:48 -0700

    pal

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,38 +2,50 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * is_palindrome - checks for list is palindrome
- * @head: head of linked list
- * Return:  boolean
+ * is_palindrome - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * Return: 1 if is palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	int len = 0, i = 0;
-	listint_t *tmp;
-	int *Ns;
+	listint_t *ojo, *ojo2;
+	int tama = 0;
+	int ciclador = 0;
+	int iterador = 0;
+	int otra = 0;
+	int cont = 0;
 
-	tmp = *head;
-	if ((*head) == NULL)
+	if (!*head)
 		return (1);
-	while (tmp->next != NULL)
+	ojo = *head;
+	ojo2 = *head;
+	while (ojo->next)
 	{
-		len++;
-		tmp = tmp->next;
+		ojo = ojo->next;
+		tama++;
 	}
-	if (len == 1)
-		return (1);
-	Ns = malloc(sizeof(int) * len);
-	tmp = *head;
-	while (tmp != NULL)
-	{
-		Ns[i] = tmp->n;
-		tmp = tmp->next;
-		i++;
-	}
-	for (i = 0; i <= len / 2; i++)
-	{
-		if(Ns[i] != Ns[len - i])
-			return (0);
+	ojo = *head;
+	ciclador = tama + 1;
+	if (tama % 2 == 0)
+		otra = tama / 2;
+	else
+		otra = (tama / 2) - 1;
+	while (iterador < ciclador)
+	{		ojo2 = ojo2->next;
+		iterador++;
+		if (iterador == tama)
+		{
+			if (ojo->n == ojo2->n)
+			{				cont++;
+				iterador = cont - 1;
+				tama--;
+				if (tama == otra)
+					break;
+				ojo2 = ojo;
+				ojo = ojo->next;
+			}			else
+				return (0);
+		}
 	}
 	return (1);
 }
