ad36eec43ddcf2fafd9057ebb4f628c045cca126
MohamedAdel89 <eng.mooh_adel@yahoo.com>
2023-07-03 12:33:18 -0700

    MT

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,86 +1,46 @@
 #!/usr/bin/python3
-
-class chess_board:
-
-    def __init__(self, N):
-        self.N = N
-        self.setted_queens = N
-        self.next_queen = [0, 0]
-
-    def create_game(self):
-        self.weights = []
-        self.setted_queens = self.N
-        for rows in range(self.N):
-            self.weights.append([None] * self.N)
-
-    def set_queen(self, position):
-        self.weights[int(position[0])][int(position[1])] = 0
-        self.position = position
-        self.setted_queens -= 1
-
-    def update_board(self):
-        sw = 0
-        actual_pos = (self.position[0] * self.N) + self.position[1]
-        for row in range(self.N):
-            for column in range(self.N):
-                tmpRow = row - self.position[0]
-                tmpCol = column - self.position[1]
-                if (abs(tmpRow) == abs(tmpCol)
-                    and row != self.position[0]
-                    and column != self.position[1]):
-                    self.weights[row][column] = -1
-                elif row == self.position[0] and column == self.position[1]:
-                    self.weights[row][column] = 0
-                elif row == self.position[0] or column == self.position[1]:
-                    self.weights[row][column] = -1
-                else:
-                    if (actual_pos < (row * self.N) + column and
-                        self.weights[row][column] != -1 and
-                        self.weights[row][column] != 0) or (
-                        self.weights[row][column] == None):
-                        if sw == 0:
-                            sw = 1
-                            self.next_queen = [row, column]
-                        self.weights[row][column] = self.setted_queens
+""" puzzle queen challenge
+"""
+from sys import argv, exit
 
 
-    def check_pos(self):
-        for row in self.weights[:]:
-            if max(row) > 0:
-                return True
+def place(N, row, col, result):
+    """ place queens recursively """
+    while col < N:
+        if isvalid(row, col, result):
+            result.append([row, col])
+            if row == N-1:
+                print(result)
+                result.pop()
+            else:
+                place(N, row+1, 0, result)
+        col += 1
+    if len(result) > 0:
+        result.pop()
+    return
+
+
+def isvalid(row, col, result):
+    """ check if the position is valid """
+    diag1 = [l[0]+l[1] for l in result]
+    diag2 = [l[1]-l[0] for l in result]
+    cols = [l[1] for l in result]
+    rows = [l[0] for l in result]
+    if row in rows or col in cols or row+col in diag1 or col-row in diag2:
         return False
+    return True
 
-from sys import argv, exit, stderr
-
-if len(argv) != 2:
-    stderr.write("Usage: nqueens N\n")
-    exit(1)
-try:
+if __name__ == "__main__":
+    length = len(argv)
+    if length != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
     N = int(argv[1])
-except:
-    stderr.write("N must be a number\n")
-    exit(1)
-if type(N) is not int:
-    stderr.write("N must be a number\n")
-    exit(1)
-elif N < 4:
-    stderr.write("N must be at least 4\n")
-    exit(1)
-
-cb = chess_board(N)
-cb.create_game()
-for row in range(N // 2):
-    for col in range(N):
-        queens_loc = []
-        cb.create_game()
-        cb.next_queen = [row, col]
-        queens_loc.append(cb.next_queen)
-        while len(queens_loc) != N:
-            cb.set_queen(cb.next_queen)
-            cb.update_board()
-#            print(cb.weights)
-            if cb.check_pos() == False:
-                break
-            queens_loc.append(cb.next_queen)
-        if len(queens_loc) == 4:
-            print(queens_loc)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+    result = []
+    place(N, 0, 0, result)
